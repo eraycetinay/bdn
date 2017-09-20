95cf597e4462c6a3da18cefe345e92aac3134b70
Eray <eraycetinay@gmail.com>
2017-09-20 23:46:03 +0300

    port

--- Code/server/app.js
+++ Code/server/app.js
@@ -21,5 +21,5 @@ app.use('/app',function(req,res,next){
 });
 app.use('*',express.static(path.join(__dirname, '/../client')));
 
-http.listen(1111);
-console.log('Magic happens at localhost:1111');
+http.listen((process.env.PORT || 1111));
+console.log('Magic happens at localhost:'+(process.env.PORT || 1111));
