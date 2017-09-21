e4a0d666f4149699307b46b52742cd404a8c6254
Eray <eraycetinay@gmail.com>
2017-09-21 19:46:10 +0300

    herokuu

--- Code/server/app.js
+++ Code/server/app.js
@@ -1,26 +1,20 @@
 var express = require('express');
-var mongoose = require('mongoose'); 
+var mongoose = require('mongoose');
 var path = require('path');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var rootAddress = path.resolve(__dirname);
 //Require ends
 
 mongoose.connect('mongodb://erayadmin:erayadmin@ds143754.mlab.com:43754/blooddonor', {
-  useMongoClient: true
-}); 
- 
-require('./blooddonor/socket.router.js')(io);
+    useMongoClient: true
+});
 
-app.use('/assets',function(req,res,next){  
-	console.log(path.join(__dirname, '/../client'+req.originalUrl));
-	console.log(path.join(__dirname, '/../client'+req.originalUrl));
-	res.sendFile(path.join(__dirname, '/../client'+req.originalUrl));
-});
-app.use('/app',function(req,res,next){ 
-	res.sendFile(path.join(__dirname, '/../client'+req.originalUrl));
-});
-app.use('*',express.static(path.join(__dirname, '/../client')));
+require('./blooddonor/socket.router.js')(io);
+  
+app.use(express.static(path.join(path.resolve(__dirname), '/../client')));
+
 
 http.listen((process.env.PORT || 1111));
-console.log('Magic happens at localhost:'+(process.env.PORT || 1111));
+console.log('Magic happens at localhost:' + (process.env.PORT || 1111));
